634d7c4d7bb5d221688b7cb4e1d7ce9d9212132d
cooper <tokyolabs@protonmail.com>
2023-08-03 10:16:14 -0400

    wip

---
+++ read_file.py
@@ -0,0 +1,9 @@
+f = open("inputFile.txt", "r")
+# count = 0
+for line in f:
+    line_split = line.split()
+    if line_split[2] == "P":
+        print(line)
+    # print(str(count) + line)
+    # count = count + 1
+f.close()
