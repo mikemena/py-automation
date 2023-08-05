30e5213187fb36823cd6731601d6f5f3ffe99789
cooper <tokyolabs@protonmail.com>
2023-08-04 20:29:55 -0400

    read and write file

--- read_file.py
+++ read_file.py
@@ -1,9 +1,16 @@
 f = open("inputFile.txt", "r")
+passFile = open("PassFile.txt", "w")
+failFile = open("FailFile.txt", "w")
 # count = 0
 for line in f:
     line_split = line.split()
     if line_split[2] == "P":
-        print(line)
+        passFile.write(line)
+        # print(line)
+    elif line_split[2] == "F":
+        failFile.write(line)
     # print(str(count) + line)
     # count = count + 1
 f.close()
+passFile.close()
+failFile.close()
