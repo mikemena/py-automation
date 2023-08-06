2f4f2d9032ee93de16917c63fc12bde2ba11db5b
cooper <tokyolabs@protonmail.com>
2023-08-05 20:39:57 -0400

    wip

--- organizing_directory.py
+++ organizing_directory.py
@@ -20,11 +20,19 @@ def pickDirectory(value):
 
 
 def organizeDirectory():
+    # get all objects in folder
     for item in os.scandir():
+        # get file path for each file
         filePath = Path(item)
+        # isolate the suffix to see what directory it needs to go to
         filetype = filePath.suffix.lower()
+        # use pickDirectory function and pass in the suffix
         directory = pickDirectory(filetype)
+        # cast directory to a path for file movement
         directoryPath = Path(directory)
+        # if the directory the file maps to does not exist, then create it
         if directoryPath.is_dir() != True:
             directoryPath.mkdir()
+        # moving file happens in both cases [directory exist and directory does not exist]
+        # so this is outside of if statement
         filePath.rename(directoryPath.joinpath(filePath))
