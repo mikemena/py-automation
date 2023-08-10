a9326e4b59410fd4f56e724bdf2c14e3905b7d0b
cooper <tokyolabs@protonmail.com>
2023-08-10 14:44:44 -0400

    web scrape

--- web_scraping.py
+++ web_scraping.py
@@ -5,4 +5,6 @@ url = "https://quotes.toscrape.com/"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
-print(soup)
+quotes = soup.find_all("span", class_="text")
+for quote in quotes:
+    print(quote.text)
