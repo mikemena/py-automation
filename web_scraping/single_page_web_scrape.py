b3e6f402cda2396f509d3612857129ceb87e521b
cooper <tokyolabs@protonmail.com>
2023-08-10 18:35:46 -0400

    scrape

--- web_scraping/single_page_web_scrape.py
+++ web_scraping/single_page_web_scrape.py
@@ -4,3 +4,6 @@ import requests
 url = "https://scrapingclub.com/exercise/list_basic/?page=1"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
+items = soup.find_all("div", class_="w-full rounded border")
+for i in items:
+    
