ee6a29c8ebee87fcc3d0e7c42573b82f08087d3d
cooper <tokyolabs@protonmail.com>
2023-08-10 20:35:12 -0400

    scrape single page

--- web_scraping/single_page_web_scrape.py
+++ web_scraping/single_page_web_scrape.py
@@ -4,6 +4,10 @@ import requests
 url = "https://scrapingclub.com/exercise/list_basic/?page=1"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
-items = soup.find_all("div", class_="w-full rounded border")
+items = soup.find_all("div", class_="p-4")
+count = 1
 for i in items:
-    
+    itemName = i.find("a").text
+    itemPrice = i.find("h5").text
+    print("%s ) Price: %s, Item Name: %s" % (count, itemPrice, itemName))
+    count = count + 1
