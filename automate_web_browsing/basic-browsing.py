ef2d253c7939823237c0fc1dfdd061a79023bd66
cooper <tokyolabs@protonmail.com>
2023-08-11 21:13:29 -0400

    wip

--- automate_web_browsing/basic-browsing.py
+++ automate_web_browsing/basic-browsing.py
@@ -1,13 +1,14 @@
-# https://www.techlistic.com/2020/07/automation-testing-demo-websites.html
-# https://www.techlistic.com/p/selenium-practice-form.html
-
+# https://www.selenium.dev/documentation/webdriver/elements/interactions/
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome()
-driver.get("https://www.techlistic.com/p/selenium-practice-form.html")
-name_field = driver.find_element(
-    "xpath", "//firstname[text()=’About Software Test Academy’]"
-)
-name_field.send_keys("test")
-print(name_field)
+driver.get("https://www.selenium.dev/selenium/web/inputs.html")
+
+# Clear field to empty it from any previous data
+driver.find_element(By.NAME, "email_input").clear()
+
+# Enter Text
+driver.find_element(By.NAME, "email_input").send_keys("test@localhost.dev")
